cda1c46c1fcfd81915483b9200cc88be80549aeb
Turchanov Denis <turchanovdenis786@gmail.com>
2022-07-18 18:07:44 +0200

    about section

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(()=>{
+    $('.nft-arts__slider').slick({
+
+        slidesToShow: 3,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="../img/arrow_l.png"> </button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/arrow_r.png"></button>'
+    });
+  })
+  
