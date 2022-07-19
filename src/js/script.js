5cdfb3ec552ba990818c6b3a69b7940d1bdbbdda
Turchanov Denis <turchanovdenis786@gmail.com>
2022-07-19 23:33:34 +0200

    update
    add fonts
    translate footer lists into links
    make social media icons links
    improve scrollbar

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,18 @@ $(document).ready(()=>{
         nextArrow: '<button type="button" class="slick-next"><img src="../img/arrow_r.png"></button>'
     });
   })
-  
+  $(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
