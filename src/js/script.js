3b744c57065916f2410219a38c0731aa3ac736f4
Turchanov Denis <turchanovdenis786@gmail.com>
2022-10-11 17:23:28 +0200

    adaptived footer

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(()=>{
     $('.nft-arts__slider').slick({
 
         slidesToShow: 3,
-        prevArrow: '<button type="button" class="slick-prev"> <img src="../img/arrow_l.png"> </button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/arrow_r.png"></button>'
+        prevArrow: '<button type="button" class="slick-prev"> <img src="img/arrow_l.png"> </button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow_r.png"></button>'
     });
   })
   $(document).ready(function(){
